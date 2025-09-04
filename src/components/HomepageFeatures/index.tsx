1d649d0d1b83fbce9ad133445ef9e4c14b46b31f
Zaela Rose <zaela.r@tamakicontrol.com>
2025-09-04 11:55:50 -0500

    Replace docusaurus branding on main page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,7 +11,7 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Ignition Focused',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/Logo-Ignition-Check.svg').default,
     description: (
       <>
         The Tamaki MES is built with Ignition in mind. It plugs directly in and lets you manage all configuration using Ignition's built-in tools. 
@@ -20,7 +20,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Highly Extendable',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/Extensible.svg').default,
     description: (
       <>
         The Tamaki MES Module comes with a wide range of baked-in MES features but is built with a modular design in mind. This allows you to extend the module with your own custom functionality.
@@ -29,10 +29,10 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Powered by Spring Boot',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/Spring-Boot.svg').default,
     description: (
       <>
-        something something something.
+        Spring Boot provides access to pre-built components and security features, eliminating the need to build everything from scratch. The result is a modern, maintainable solution that will evolve efficiently with customer needs, minimizing technical debt.
       </>
     ),
   },
@@ -42,7 +42,7 @@ function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <Svg className={styles.featureSvg} style={{height: '5rem'}} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
